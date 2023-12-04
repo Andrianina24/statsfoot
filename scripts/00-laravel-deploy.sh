dbe6ee635443c735dd35cf8b21b9ed134045675c
Andrianina24 <rakotozananya5@gmail.com>
2023-12-04 14:11:22 +0300

    final

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,8 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+# echo "generating application key..."
+# php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
